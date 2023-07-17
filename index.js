0490de939bf3bc96d8a6eca277bfc6b2897793fc
ysfjr1 <yohanansem@gmail.com>
2023-07-17 18:28:35 +0300

    third commit

--- index.js
+++ index.js
@@ -12,72 +12,66 @@ const accounts = [
 ];
 
 // Function to log in and check for appointment updates
-function checkAppointmentUpdates(account, callback) {
-  // Log in to the website
-  axios
-    .post("https://ais.usvisa-info.com/en-et/niv/users/sign_in", {
-      user: {
-        login: account.username,
-        password: account.password,
-      },
-    })
-    .then((response) => {
-      const cookies = response.headers["set-cookie"];
+async function checkAppointmentUpdates(account) {
+  try {
+    // Log in to the website
+    const loginResponse = await axios.post(
+      "https://ais.usvisa-info.com/en-us/niv/users/sign_in",
+      {
+        user: {
+          login: account.username,
+          password: account.password,
+        },
+      }
+    );
 
-      // Make a GET request to the appointment page
-      axios
-        .get(
-          "https://ais.usvisa-info.com/en-et/niv/schedule/32514956/appointment/dates",
-          {
-            headers: {
-              Cookie: cookies,
-            },
-          }
-        )
-        .then((response) => {
-          const $ = cheerio.load(response.data);
+    const cookies = loginResponse.headers["set-cookie"];
 
-          // Extract appointment availability information
-          const availableAppointments = [];
-          $(".date.status-appointment").each((index, element) => {
-            availableAppointments.push($(element).attr("data-date"));
-          });
+    // Make a GET request to the appointment page
+    const appointmentResponse = await axios.get(
+      "https://ais.usvisa-info.com/en-us/niv/schedule/32514956/appointment/dates",
+      {
+        headers: {
+          Cookie: cookies,
+        },
+      }
+    );
 
-          // Return the update
-          callback(null, {
-            account: account.username,
-            appointments: availableAppointments,
-          });
-        })
-        .catch((error) => {
-          callback(error);
-        });
-    })
-    .catch((error) => {
-      callback(error);
+    const $ = cheerio.load(appointmentResponse.data);
+
+    // Extract appointment availability information
+    const availableAppointments = [];
+    $(".date.status-appointment").each((index, element) => {
+      availableAppointments.push($(element).attr("data-date"));
     });
+
+    return {
+      account: account.username,
+      appointments: availableAppointments,
+    };
+  } catch (error) {
+    throw error;
+  }
 }
 
 // Run the scraper
-async.eachSeries(
-  accounts,
-  (account, callback) => {
-    checkAppointmentUpdates(account, (error, result) => {
-      if (error) {
-        console.error(`Error for ${account.username}: ${error.message}`);
-      } else {
-        console.log(
-          `Update for ${result.account}: ${result.appointments.length} new appointments`
-        );
-      }
-      setTimeout(callback, 30000); // Wait 30 seconds before the next login
-    });
-  },
-  (error) => {
-    if (error) {
-      console.error(`Scraping encountered an error: ${error.message}`);
-    } else {
-      console.log("Scraping completed");
+(async function () {
+  for (let i = 0; i < accounts.length; i++) {
+    const account = accounts[i];
+    try {
+      const result = await checkAppointmentUpdates(account);
+      console.log(
+        `Update for ${result.account}: ${result.appointments.length} new appointments`
+      );
+    } catch (error) {
+      console.error(`Error for ${account.username}: ${error.message}`);
     }
+    await delay(30000); // Wait 30 seconds before the next login
   }
-);
+  console.log("Scraping completed");
+})();
+
+// Helper function to introduce delay
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
