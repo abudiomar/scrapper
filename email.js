2c2f33661d6e73ef334a01c168ae01ee4714b37e
Yohanan Semere <74771878+ysfjr1@users.noreply.github.com>
2023-07-22 18:36:34 +0300

    Update email.js

--- email.js
+++ email.js
@@ -4,7 +4,7 @@ module.exports = {
     { username: "yishakdawit96@gmail.com", password: "Muazmusa@123" },
     { username: "ozakbederu@gmail.com", password: "Muazmusa@123" },
     { username: "travelmuaz99@gmail.com", password: "Muazmusa@123" },
-    { username: "semirtravelopia@gmail.com", password: "Muazmusa@123" },//
+    { username: "semirtravelopia@gmail.com", password: "Muazmusa@123" },
     { username: "abdizak234@gmail.com", password: "Muazmusa@123" },
     { username: "munibamusa22@gmail.com", password: "Muazmusa@123" },
     { username: "haseenmusa093@gmail.com", password: "Muazmusa@123" },
