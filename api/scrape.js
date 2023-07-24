61b7d76acc52aa72af2f7ff1348c976684b09059
ysfjr1 <yohanansem@gmail.com>
2023-07-24 17:00:45 +0300

    one email try

--- api/scrape.js
+++ api/scrape.js
@@ -13,7 +13,7 @@ async function start() {
   try {
     for (let i = 0; i < emails.length; i++) {
       const browser = await puppeteer.launch({
-        headless: false,
+        headless: true,
       });
 
       const page = await browser.newPage();
