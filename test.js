957eccf50ccf7311d48d38d111bf953917423c19
ysfjr1 <yohanansem@gmail.com>
2023-09-02 14:00:07 +0300

    test

---
+++ test.js
@@ -0,0 +1,84 @@
+const { emails } = require("./email");
+const puppeteer = require("puppeteer");
+const { Bot } = require("grammy");
+require("dotenv").config();
+
+const bot = new Bot(`${process.env.TELEGRAM_BOT_TOKEN}`);
+const delay = (milliseconds) =>
+  new Promise((resolve) => setTimeout(resolve, milliseconds));
+
+const CHATID = process.env.TELEGRAM_BOT_CHAT_ID;
+
+async function start() {
+
+  let date = new Date().toLocaleTimeString()
+
+    console.log("start time = " + " " + date)
+  try {
+    const browser = await puppeteer.launch({
+      headless: true,
+    });
+
+    for (let i = 0; i < emails.length; i++) {
+      const page = await browser.newPage();
+
+      await page.setDefaultNavigationTimeout(0);
+
+      await page.goto("https://ais.usvisa-info.com/en-et/niv/users/sign_in");
+
+      await page.waitForSelector(".string.email.required");
+      await page.type(".string.email.required", emails[i].username);
+      await page.type("#user_password", emails[i].password);
+      await page.waitForSelector(
+        "#sign_in_form > div.radio-checkbox-group.margin-top-30 > label > div"
+      );
+      await page.click(
+        "#sign_in_form > div.radio-checkbox-group.margin-top-30 > label > div"
+      );
+      await page.click(".simple_form.new_user p input");
+
+      await page.waitForSelector(".medium-6.columns.text-right ul li a");
+      await page.click(
+        "#main > div:nth-child(2) > div.mainContent > div:nth-child(1) > div > div > div:nth-child(1) > div.medium-6.columns.text-right > ul > li > a"
+      );
+      await delay(2000);
+      await page.waitForSelector(".fas.fa-money-bill-alt");
+
+      await delay(6000);
+
+      await page.waitForSelector(
+        "#forms > ul > li:nth-child(1) > div > div > div.medium-10.columns > p:nth-child(2) > a"
+      );
+      await page.evaluate(() =>
+        document
+          .querySelector(
+            "#forms > ul > li:nth-child(1) > div > div > div.medium-10.columns > p:nth-child(2) > a"
+          )
+          .click()
+      );
+
+      await page.waitForSelector(
+        "#paymentOptions > div.medium-3.column > table > tbody > tr > td.text-right"
+      );
+      const slotDate = await page.$eval(
+        "#paymentOptions > div.medium-3.column > table > tbody > tr > td.text-right",
+        (el) => el.textContent
+      );
+
+      let slot = slotDate + "Hurry up and book";
+      // await bot.api.sendMessage(CHATID, slot);
+      let date = new Date().toLocaleTimeString()
+      console.log(slot + date)
+
+      await page.close();
+    }
+
+    await browser.close();
+    console.log("Scraping completed");
+  } catch (error) {
+    console.error(error);
+    throw new Error("Internal server error");
+  }
+}
+
+start();
