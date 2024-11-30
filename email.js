93a1018e88b5f1264435fc7f46d461f398858206
abudiomar <abudi4947@gmail.com>
2024-11-30 22:12:07 +0300

    B1 channel

--- email.js
+++ email.js
@@ -1,13 +1,18 @@
 module.exports = {
   emails: [
     { username: "enatschool1@gmail.com", password: "Muazmusa@123" },
-    { username: "yishakdawit96@gmail.com", password: "Muazmusa@123" },
+    { username: "Shopmartretha@gmail.com", password: "Muazmusa@123" },
+    { username: "zomar5070@gmail.com", password: "Muazmusa@123" },
+    { username: "abinertesfaye@gmail.com", password: "Muazmusa@123" },
+    { username: "haseenmusa093@gmail.com", password: "Muazmusa@123" },
     { username: "ozakbederu@gmail.com", password: "Muazmusa@123" },
     { username: "travelmuaz99@gmail.com", password: "Muazmusa@123" },
     { username: "semirtravelopia@gmail.com", password: "Muazmusa@123" },
-    // { username: "abdizak234@gmail.com", password: "Muazmusa@123" },
     { username: "munibamusa22@gmail.com", password: "Muazmusa@123" },
     { username: "haseenmusa093@gmail.com", password: "Muazmusa@123" },
+    { username: "yishakdawit96@gmail.com", password: "Muazmusa@123" },
+
+    ,
   ],
 };
-//
+// { username: "abdizak234@gmail.com", password: "Muazmusa@123" },
