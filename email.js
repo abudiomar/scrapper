9997024c9cc931c9bedcfbe50068fd8153551675
ysfjr1 <yohanansem@gmail.com>
2024-12-17 22:47:12 +0300

    fixed minor errors

--- email.js
+++ email.js
@@ -1,7 +1,7 @@
 module.exports = {
   emails: [
     { username: "liyabebe02@yahoo.com", password: "Semir@12345" },
-/*     { username: "saronegussie@yahoo.com", password: "Semir@12345" },
+    { username: "saronegussie@yahoo.com", password: "Semir@12345" },
     { username: "foziimohammed12@gmail.com", password: "Semir@12345" },
     { username: "mohamedbeshir345@gmail.com", password: "Semir@12345" },
     { username: "traveleurostep@gmail.com", password: "Semir@12345" },
@@ -9,6 +9,6 @@ module.exports = {
     { username: "musababubaker2@gmail.com", password: "Semir@12345" },
     { username: "mihretbahru@yahoo.com", password: "Semir@12345" },
     { username: "Bisratbelay1@yahoo.com", password: "Semir@12345" },
-    { username: "Kalmulugeta1@yahoo.com", password: "Semir@12345" }, */
+    { username: "Kalmulugeta1@yahoo.com", password: "Semir@12345" },
   ],
 };
